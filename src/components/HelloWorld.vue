8a00032d8ff0e1757aaf42c91d51dd2641a5520c
ARMIN1313 <96407325+ARMIN1313@users.noreply.github.com>
2022-05-13 21:31:03 +0430

    add digit(7,8,9)and div and clear

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,25 @@
-<template>
+ <template>
   <div>
     <table id="calculator" dir="ltr" style="text-align: center; margin: auto">
-  <tr>
-  <!-- table row -->
-  <td colspan="5">
-  <!-- span = امتداد داشتن -->
-
-</template>
-  
+      <tr>
+        <!-- table row -->
+        <td colspan="5">
+          <!-- span = امتداد داشتن -->
+          <input
+            v-model="screen"
+            dir="ltr"
+            id="result"
+            type="number"
+            style="padding: 5px; font-weight: bold; text-align: right"
+          />
+      <td>
+        <!-- table data -->
+        </tr>
+      <tr>
+        <td><button @click="digit(7)" class="digit">7</button></td>
+        <td><button @click="digit(8)" class="digit">8</button></td>
+        <td><button @click="digit(9)" class="digit">9</button></td>
+        <td><button @click="div" id="div">/</button></td>
+        <td><button @click="clear" id="clear">C</button></td>
+      </tr>
+ </template>
