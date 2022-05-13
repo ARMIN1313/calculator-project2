dd8377dac40836bfceb0a962f987d6ae415e75df
ARMIN1313 <96407325+ARMIN1313@users.noreply.github.com>
2022-05-13 19:49:25 +0430

    Add colspan

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <table id="calculator" dir="ltr" style="text-align: center; margin: auto">
-  
+  <tr>
+  <!-- table row -->
+  <td colspan="5">
+  <!-- span = امتداد داشتن -->
+
 </template>
   
